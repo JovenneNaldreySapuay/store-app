10b22067f761db7b7fcf36183bcf875c7589e9a2
JovenneNaldreySapuay <jovenneregs@gmail.com>
2020-08-21 21:05:34 +0800

    initial commit

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - production keys here!!
 
 module.exports = {
-	mlabUrl: process.env.MONGO_URL,
+	shopUrl: process.env.MONGO_URL,
 	jwtSecret: process.env.JWT_SECRET,
 	host: process.env.HOST,
 	emailHost: process.env.EMAIL_HOST,
