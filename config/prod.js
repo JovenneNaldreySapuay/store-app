9b2f8d0d93cee09b1031616d033d3d46075a8cc3
JovenneNaldreySapuay <jovenneregs@gmail.com>
2020-08-30 22:37:24 +0800

    config keys

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,11 @@
-// prod.js - production keys here!!
-
 module.exports = {
 	shopUrl: process.env.MLAB_URL,
 	jwtSecret: process.env.JWT_SECRET,
-	host: process.env.HOST,
+	host: process.env.HOST, 
+	stripePublishableKey: process.env.STRIPE_PUBLISH_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 	emailHost: process.env.EMAIL_HOST,
 	emailPort: process.env.EMAIL_PORT,
 	emailUser: process.env.EMAIL_USER,
-	emailPass: process.env.EMAIL_PASS 
+	emailPass: process.env.EMAIL_PASS, 
 };
