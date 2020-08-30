10e42cedba5a24ca997f127e4421e8d4d1386e0a
JovenneNaldreySapuay <jovenneregs@gmail.com>
2020-08-30 22:11:41 +0800

    Adding stripe payment form element

--- app-client/src/components/checkout/CheckoutPage.js
+++ app-client/src/components/checkout/CheckoutPage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {CardElement} from '@stripe/react-stripe-js';
 
+import CardInput from './CardInput';
 import { fetchCartByUser } from '../../actions/cart';
 import { addCheckoutProduct } from '../../actions/checkout';
 
@@ -31,16 +34,19 @@ class CheckoutPage extends Component {
   	shipping_option: '',
   	payment_method: '',
 	redirect: false,
+	email: '',
+	isProcessing: false,
   	// loading: false,
 	// errors: {}
   };
 
-  handleOrderClick = (e) => {
+  handleOrderClick = async (e) => {
 	e.preventDefault();
+	
+	const {stripe, elements} = this.props;
 
-	const { shipping_fee } = this.props;
-	const { products } = this.props;
-	const { message, shipping_option, payment_method } = this.state;
+	const { userid, products, shipping_fee } = this.props;
+	const { message, shipping_option, payment_method, email } = this.state;
 	
 	let totalArray = [];
 	let subtotal = 0;
@@ -62,6 +68,11 @@ class CheckoutPage extends Component {
 		return totalQty;
 	})
 
+	// stripe not loaded... yet
+	if (!stripe || !elements) {
+    	return;
+    }
+
   	const checkoutProduct = {
   		products: products,
   		shipping_option: shipping_option,
@@ -70,172 +81,215 @@ class CheckoutPage extends Component {
   		shipping_fee: shipping_fee,
   		quantity: totalQty,
   		total: total,
+  		userid: userid,
+  		email: email,
   	}
   	
-	// console.log("Checkout Data:", checkoutProduct);
+	console.log("Checkout Data:", checkoutProduct);
 	
-	this.props.addCheckoutProduct(checkoutProduct);
+	this.setState({ isProcessing: true });
+	
+	const response = await this.props.addCheckoutProduct(checkoutProduct);
 
-	this.setState({ 
-		redirect: true
+	console.log("Client Response", response);
+
+	const clientSecret = response.data['client_secret'];
+
+	console.log("clientSecret", clientSecret);
+
+	const result = await stripe.confirmCardPayment(clientSecret, {
+		payment_method: {
+			card: elements.getElement(CardElement),
+			billing_details: {
+				email: email, 
+			},
+		},
 	});
 
+	console.log("confirmCardPayment result:", result);
+
+	if (result.error) {
+		// Show error to your customer (e.g., insufficient funds)
+		console.log(result.error.message);
+
+		this.setState({ isProcessing: false });
+		this.setState({ redirect: false });
+
+	} else {
+		// The payment has been processed!
+		if (result.paymentIntent.status === 'succeeded') {
+			console.log('Money is in the bank!');
+
+			this.setState({ redirect: true });
+		}
+	}	
   }	
 
-  componentDidMount() {
-	const { _id } = this.props.user;
-    this.props.fetchCartByUser(_id);
-  }
+	componentDidMount() {
+		const { _id } = this.props.user;
+		this.props.fetchCartByUser(_id);
+	}
 
-  handleOnChange = (event) => {
-  this.setState({
-   	[event.target.name]: event.target.value,
-  })
-}
+	handleOnChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		})
+	}
   
+render() {
 
-  render() {
-
-  const { redirect } = this.state;
   const { products, user } = this.props;
-  
-  console.log("CheckoutPage", this.props);
-
   const subTotal = computeCartTotal(products);
   const total = subTotal + this.props.shipping_fee;
-
   const fullAddress = `${user.address}, ${user.city}, ${user.province}, ${user.country}, ${user.zipcode}`;
-  return (
-	     <div className="container mx-auto w-full bg-white pt-3" style={{ minHeight: '100vh' }}>
-	     <div className="p-5 mt-2">
-			<h1 className="text-center uppercase font-bold text-xl tracking-wide">
-				Checkout
-			</h1>
-		 </div>
+  
+  // console.log("CheckoutPage", this.props);
+  
+  return (  		 
+		  <div className="container mx-auto w-full bg-white pt-3" style={{ minHeight: '100vh' }}>
+		     <div className="p-5 mt-2">
+				<h1 className="text-center uppercase font-bold text-xl tracking-wide">
+					Checkout
+				</h1>
+			 </div>
 
-		  { products.length > 0 ? ( 
-		 <div className="w-10/12 mx-auto">  
-	     <div className="delivery-address p-3 my-3 bg-blue-100 mb-5">
-	  	 	<h2 className="text-black pb-2 font-semibold">Delivery Address</h2>
-	  	 	<p>
-	  	 	<span className="font-semibold">
-	  	 	{user.fullName}{' '}({user.phone}){' '}
-	  	 	</span> 
-	  	 	<span className="text-gray-700">
-	  	 	{fullAddress}
-	  	 	</span>
-	  	 	</p>
-	  	 </div>
-
-	       <h1 className="text-center font-semibold text-xl tracking-wide">Products Ordered</h1>
-
-	       <table border="1" className="checkout-table border w-full mt-3 mb-2">
-	       <thead>
-	         <tr>
-	            <th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide"></th>
-	            <th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Item Name</th>
-	            <th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Unit Price</th>
-	            <th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Quantity</th>
-	            <th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Item Subtotal</th>
-	        </tr>
-	       </thead>
-	       <tbody>
-	       {  
-	          products.map(item => {
-	            return (
-	              <tr key={item._id}>
-	                <td className="p-1 border">
-	                <img 
-					className="border border-grey-200 m-auto"
-					title={item.title}
-					src={item.image} 
-					alt={item.title} 
-					width={50} 
-					/>
-	                </td>
-	                <td className="p-1 border text-center">{item.title}</td>
-	                <td className="p-1 border text-center">${item.price}</td>
-	                <td className="p-1 border text-center">{item.quantity}</td>
-	                <td className="p-1 border text-center">${item.total}</td>
-	              </tr>
-	            )})
-	        }
-	        </tbody>         
-	        </table>
-	        
-	        <div className="flex justify-end">
-		        <div className="border border-grey-200 mb-6">	
-					<div className="leave-message p-3">
-					<label htmlFor="message">Leave a message:&nbsp;</label>
-					<input className="border p-2" type="text" name="message" id="message" placeholder="Your message..." onChange={this.handleOnChange} />
-					</div>  
-
-					<div onChange={this.handleOnChange} className="shipping-option p-3">
-					<p style={{fontWeight: 'bold'}}>Shipping Option:</p>
-					<p>
-					<input type="radio" id="std" name="shipping_option" value="Standard Delivery" />
-					<label htmlFor="std">&nbsp;Standard Delivery</label>
-					</p>  
-					<p>
-					<input type="radio" id="ninjavan" name="shipping_option" value="Ninja Van" />
-					<label htmlFor="ninjavan">&nbsp;Ninja Van</label>
-					</p>  
+			  { products.length > 0 ? ( 
+			 	<div className="w-10/12 mx-auto">  
+					<div className="delivery-address p-3 my-3 bg-blue-100 mb-5">
+						<h2 className="text-black pb-2 font-semibold">Delivery Address</h2>
+						<p>
+						<span className="font-semibold">
+						{user.fullName}{' '}({user.phone}){' '}
+						</span> 
+						<span className="text-gray-700">
+						{fullAddress}
+						</span>
+						</p>
 					</div>
 
-					<div onChange={this.handleOnChange} className="payment-method p-3">
-					<p style={{fontWeight: 'bold'}}>Payment Method:</p>
-					<p>
-					<input type="radio" id="cod" name="payment_method" value="COD" />
-					<label htmlFor="cod">&nbsp;COD</label>
-					</p>  
-					<p>
-					<input type="radio" id="credit" name="payment_method" value="Credit Card" />
-					<label htmlFor="credit">&nbsp;Credit Card</label>
-					</p>
-					<p>
-					<input type="radio" id="paypal" name="payment_method" value="PayPal" />
-					<label htmlFor="paypal">&nbsp;PayPal</label>
-					</p>
-					</div>
+		       		<h1 className="text-center font-semibold text-xl tracking-wide">Products Ordered</h1>
 
-					<div className="payment-summary bg-gray-200 p-3">
-					<p>Merchandise Subtotal: <span className="font-semibold">${subTotal}</span></p>
-					<p>Shipping Total: <span className="font-semibold">${this.props.shipping_fee}</span></p>
-					<p className="total-payable">Total Payment: <span className="font-semibold">${total}</span></p>
-					</div>
+					<table border="1" className="checkout-table border w-full mt-3 mb-2">
+						<thead>
+						<tr>
+						<th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide"></th>
+						<th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Item Name</th>
+						<th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Unit Price</th>
+						<th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Quantity</th>
+						<th className="p-2 text-xs text-gray-900 uppercase font-bold tracking-wide">Item Subtotal</th>
+						</tr>
+						</thead>
+						<tbody>
+						{  
+						products.map(item => {
+						return (
+						<tr key={item._id}>
+						<td className="p-1 border">
+						<img 
+						className="border border-grey-200 m-auto"
+						title={item.title}
+						src={item.image} 
+						alt={item.title} 
+						width={50} 
+						/>
+						</td>
+						<td className="p-1 border text-center">{item.title}</td>
+						<td className="p-1 border text-center">${item.price}</td>
+						<td className="p-1 border text-center">{item.quantity}</td>
+						<td className="p-1 border text-center">${item.total}</td>
+						</tr>
+						)})
+						}
+						</tbody>         
+					</table>
+		        
+					<div className="flex justify-end">
+						<div className="border border-grey-200 mb-6 w-6/12">	
+						<div className="leave-message px-3">
+						<label htmlFor="message">Leave a message:&nbsp;</label>
+						<input className="border p-2" type="text" name="message" id="message" placeholder="Your message..." onChange={this.handleOnChange} />
+						</div>  
 
-					<div className="payment-cta mt-1 mb-5 mr-2 flex justify-end">
-					<button 
-					onClick={this.handleOrderClick} 
-					className="btn mt-3"
-					>Place Order
-					</button>
-					{ redirect && ( 
-					<Redirect to="/shop" /> 
-					)}
+						<div onChange={this.handleOnChange} className="shipping-option px-3">
+						<p style={{fontWeight: 'bold'}}>Shipping Option:</p>
+						<p>
+						<input type="radio" id="std" name="shipping_option" value="Standard Delivery" />
+						<label htmlFor="std">&nbsp;Standard Delivery</label>
+						</p>  
+						<p>
+						<input type="radio" id="ninjavan" name="shipping_option" value="Ninja Van" />
+						<label htmlFor="ninjavan">&nbsp;Ninja Van</label>
+						</p>  
+						</div>
+
+						<div onChange={this.handleOnChange} className="payment-method p-3">
+						<p style={{fontWeight: 'bold'}}>Payment Method:</p>
+						<p>
+						<input type="radio" id="cod" name="payment_method" value="COD" />
+						<label htmlFor="cod">&nbsp;COD</label>
+						</p>  
+						<p>
+						<input type="radio" id="credit" name="payment_method" value="Credit Card" />
+						<label htmlFor="credit">&nbsp;Credit Card</label>
+						</p>
+						<p>
+						<input type="radio" id="paypal" name="payment_method" value="PayPal" />
+						<label htmlFor="paypal">&nbsp;PayPal</label>
+						</p>
+						</div>
+
+						<div className="payment-summary bg-gray-200 p-3">
+							<p>Merchandise Subtotal: <span className="font-semibold">${subTotal}</span></p>
+							<p>Shipping Total: <span className="font-semibold">${this.props.shipping_fee}</span></p>
+							<p className="total-payable mb-4">Total Payment: <span className="font-semibold">${total}</span></p>
+							
+							<div className="stripe-section">	
+								<input className="border p-2" type="text" name="email" id="email" placeholder="email@email.com" onChange={this.handleOnChange} />
+								<CardInput />
+							</div>	
+						
+						</div>
+
+						<div className="payment-cta mt-1 mb-5 mr-2 flex justify-end">
+
+						<button 
+							onClick={this.handleOrderClick} 
+							className="btn mt-3"
+							>{this.state.isProcessing ? "Processing..." : "Place Order"}
+						</button>
+
+						{this.state.redirect && <Redirect to="/success" />}
+
+						</div> 
+						</div> 
 					</div> 
-		        </div> 
-	        </div> 
 
-		    </div>
-		    )
-	    	: (
-	    	 <div>
-	    		<h1 className="text-center mb-2">No products to checkout</h1>	
-	    		<p className="text-center"><Link to="/"><button className="btn">Shop Now</button></Link></p>
-	    	 </div>
-	    	)}
-	     </div>   
-  	)
+			    </div>
+			    )
+		    	: (
+		    	 <div>
+		    		<h1 className="text-center mb-2">No products to checkout</h1>	
+		    		<p className="text-center"><Link to="/"><button className="btn">Shop Now</button></Link></p>
+		    	 </div>
+		    	) 
+		    }
+		     </div>   		
+		)
 	}
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
 	return {
 		products: state.cart.items,
+		userid: state.auth._id,
 		user: state.auth,
 		shipping_fee: 5
 	};
 }
 
-export default connect(mapStateToProps, { fetchCartByUser, addCheckoutProduct })(CheckoutPage);
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  addCheckoutProduct: addCheckoutProduct,
+  fetchCartByUser: fetchCartByUser
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutPage);
